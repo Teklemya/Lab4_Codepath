6105f910a5466169ebc855c4d122c5f6ca155bd7
Teklemya <Teklemya@mail.uc.edu>
2024-10-08 22:41:22 -0500

    CURRENT IMAGE

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -29,9 +29,32 @@ function App() {
         inputs[key] = defaultValues[key]
       }
     }
-  }
+  } 
+  const makeQuery = () => {
+  let wait_until = "network_idle";
+  let response_type = "json";
+  let fail_on_status = "400%2C404%2C500-511";
+  let url_starter = "https://";
+  let fullURL = url_starter + inputs.url;
+  let query = `https://api.apiflash.com/v1/urltoimage?access_key=${Access_key}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+  callAPI(query).catch(console.error);}
   };
- 
+
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const callAPI = async (query) => {
+    const response = await fetch(query);
+    const json = await response.json();
+    if (json.url == null) {
+      alert("Oops! Something went wrong with that query, let's try again!")
+        }
+    else {
+      setCurrentImage(json.url);
+    }
+  }
+  
+
+
   return (
     <div className="whole-page">
     <h1>Build Your Own Screenshot! 📸</h1>
